89082ff7ca35c573163a98a068f8e414fd01741f
Eric Stiens <estiens@gmail.com>
2014-06-13 22:58:04 -0500

    add rails admin for quick/dirty admin tasks

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+gem 'rails_admin'
+
 group :development do
   gem 'pry-rails'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 WorldCupJson::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
