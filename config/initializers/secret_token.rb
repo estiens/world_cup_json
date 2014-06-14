bed20185462ef404677131e1a760c8f0f818af3d
Eric Stiens <estiens@gmail.com>
2014-06-13 22:04:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldCupJson::Application.config.secret_key_base = '976ddc7cfde7ea24d402c843ed69ffc3d730dddfa1651b1d60a0795cf7c3b5c9f963d49f5fa6df83a2e95a99fb4f04e8be2dc40e693f192154ddbef06203b856'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorldCupJson::Application.config.session_store :cookie_store, key: '_world_cup_json_session'
