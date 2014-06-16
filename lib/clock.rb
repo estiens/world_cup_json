5faad2aa1ae7273842b5188b03bd623a91060116
Eric Stiens <estiens@gmail.com>
2014-06-16 12:00:46 -0500

    clockwork job working

--- lib/clock.rb
+++ lib/clock.rb
@@ -17,4 +17,12 @@ module Clockwork
     rake['fifa:get_all_matches'].invoke
   }
 
+  every(5.minutes, "scrape_groups") {
+    rake = Rake::Application.new
+    Rake.application = rake
+    Rake::Task.define_task(:environment)
+    load "#{Rails.root}/lib/tasks/group_scraper.rake"
+    rake['fifa:get_group_results'].invoke
+  }
+
 end
