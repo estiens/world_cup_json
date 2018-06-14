ffd6cc8b14be47a7783289284e4a2e738ffabd49
estiens <eric@softwareforgood.com>
2018-06-14 04:32:14 -0500

    quick and dirty

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ WorldCupJson::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get '/teams', to: 'teams#index'
-  get '/teams/results', to: 'teams#results'
-  get '/teams/group_results', to: 'teams#group_results'
-  get '/matches', to: 'matches#index'
-  get '/matches/complete', to: 'matches#complete'
-  get '/matches/current', to: 'matches#current'
-  get '/matches/future', to: 'matches#future'
-  get '/matches/country', to: 'matches#country'
-  get '/matches/today', to: 'matches#today'
-  get '/matches/tomorrow', to: 'matches#tomorrow'
+  get '/teams', to: 'teams#index', defaults: { format: 'json' }
+  get '/teams/results', to: 'teams#results', defaults: { format: 'json' }
+  get '/teams/group_results', to: 'teams#group_results', defaults: { format: 'json' }
+  get '/matches', to: 'matches#index', defaults: { format: 'json' }
+  get '/matches/complete', to: 'matches#complete', defaults: { format: 'json' }
+  get '/matches/current', to: 'matches#current', defaults: { format: 'json' }
+  get '/matches/future', to: 'matches#future', defaults: { format: 'json' }
+  get '/matches/country', to: 'matches#country', defaults: { format: 'json' }
+  get '/matches/today', to: 'matches#today', defaults: { format: 'json' }
+  get '/matches/tomorrow', to: 'matches#tomorrow', defaults: { format: 'json' }
 
   # You can have the root of your site routed with "root"
   root 'static#index'
