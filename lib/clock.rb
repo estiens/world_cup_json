3663d47d58d5608471113e355783d5c98bf28943
estiens <eric@softwareforgood.com>
2018-06-15 08:38:32 -0500

    try slowing down event scraper

--- lib/clock.rb
+++ lib/clock.rb
@@ -8,12 +8,16 @@ module Clockwork
     puts "Running #{job}"
   end
 
-  every(1.minute, "scrape_matches") {
-    rake = Rake::Application.new
-    Rake.application = rake
-    Rake::Task.define_task(:environment)
-    load "#{Rails.root}/lib/tasks/match_scraper.rake"
-    rake['fifa:get_all_matches'].invoke
-    rake['fifa:get_events'].invoke
-  }
+  # every(1.minute, "scrape_matches") {
+  #   rake = Rake::Application.new
+  #   Rake.application = rake
+  #   Rake::Task.define_task(:environment)
+  #   load "#{Rails.root}/lib/tasks/match_scraper.rake"
+  #   rake['fifa:get_all_matches'].invoke
+  #   rake['fifa:get_events'].invoke
+  # }
+
+  every(45.seconds, 'Get Matches') { `rake fifa:get_all_matches` }
+
+  every(90.seconds, 'Get Events') { `rake fifa:get_events` }
 end
