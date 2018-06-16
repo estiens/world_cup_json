84c1fa64d492f8e446b9fd373ec45b461b12632e
estiens <eric@softwareforgood.com>
2018-06-16 10:35:47 -0500

    add redis for rack:attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,9 @@
-Rack::Attack.throttle("requests by ip", limit: 5, period: 2) do |request|
-  request.ip
+if ENV['REDISTOGO_URL']
+  redis_client = Redis.connect(url: ENV['REDISTOGO_URL'])
+  Rack::Attack.cache.store = Rack::Attack::StoreProxy::RedisStoreProxy.new(redis_client)
 end
+
+Rack::Attack.throttle('requests by ip', limit: 5, period: 2, &:ip)
 
 Rack::Attack.throttled_response = lambda do |env|
   now = Time.now
@@ -12,5 +15,5 @@ Rack::Attack.throttled_response = lambda do |env|
     'X-RateLimit-Reset' => (now + (match_data[:period] - now.to_i % match_data[:period])).to_s
   }
 
-  [ 429, headers, ["Throttled\n"]]
+  [429, headers, ["Throttled\n"]]
 end
