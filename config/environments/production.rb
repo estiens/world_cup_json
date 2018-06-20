eebad1cd260ca5cfca9c9547cb2d7bbfac7d84e8
estiens <eric@softwareforgood.com>
2018-06-19 19:27:19 -0500

    cache failure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,7 @@ WorldCupJson::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  if ENV['REDISTOGO_URL']
-    config.cache_store = :redis_store, ENV['REDISTOGO_URL'], {pool_size: 10, pool_timeout: 10}
-  end
+  config.cache_store = :redis_store, ENV['REDISTOGO_URL'] if ENV['REDISTOGO_URL']
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
