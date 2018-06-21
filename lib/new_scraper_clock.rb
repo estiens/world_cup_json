bbc9d570644b7f02896a869b7eecc1196bb0292b
estiens <eric@softwareforgood.com>
2018-06-21 06:58:51 -0500

    fix scraper runner

--- lib/new_scraper_clock.rb
+++ lib/new_scraper_clock.rb
@@ -6,24 +6,15 @@ require File.expand_path('../config/environment', __dir__)
 require 'rake'
 
 module Clockwork
-  def runner
+  every(25.seconds, 'scrapers on lock') do
     @runner ||= Scrapers::ScraperTasks.new
-  end
-
-  def game_time_stuff
     runner.check_for_live_game unless Match.in_progress.count.positive?
     runner.scrape_for_events
-  end
-
-  def grab_stats
-    runner.scrape_for_stats
-  end
-
-  every(25.seconds, 'scrapers on lock') do
     game_time_stuff
   end
 
   every(70.seconds, 'stats scraper fire') do
-    grab_stats
+    @runner ||= Scrapers::ScraperTasks.new
+    runner.scrape_for_stats
   end
 end
