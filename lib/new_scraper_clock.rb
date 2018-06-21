5ff1d72c526af7d7ed8598dfb162bb6735eb1655
estiens <eric@softwareforgood.com>
2018-06-21 13:44:59 -0500

    hourly and nightly cleanup tasks

--- lib/new_scraper_clock.rb
+++ lib/new_scraper_clock.rb
@@ -5,8 +5,22 @@ require File.expand_path('../config/boot',        __dir__)
 require File.expand_path('../config/environment', __dir__)
 require 'rake'
 
+def random_interval
+  seconds = rand(25..50)
+  seconds.seconds
+end
+
 module Clockwork
-  every(45.seconds, 'scrapers on lock') do
+  seconds = rand(20..50)
+  every(random_interval, 'scrapers on lock') do
     `rake scraper:run_scraper`
+  end
+
+  every(1.hour, 'hourly cleanup') do
+    `rake scraper:hourly_cleanup`
+  end
+
+  every(12.hours, 'nightly_cleanup') do
+    `rake scraper:nightly_cleanup`
   end
 end
--- lib/tasks/new_scraper.rake
+++ lib/tasks/new_scraper.rake
@@ -1,6 +1,14 @@
 namespace :scraper do
   desc 'try running as a rake task til we have sidekiq'
   task run_scraper: :environment do
-    Scrapers::ScraperTasks.scrape_for_goals
+    Scrapers::ScraperTasks.scrape_your_heart_out
+  end
+
+  task hourly_cleanup: :environment do
+    Scrapers::ScraperTasks.hourly_cleanup
+  end
+
+  task hourly_cleanup: :environment do
+    Scrapers::ScraperTasks.nightly_cleanup
   end
 end
