8587d7e9936bf9d524c3078dd23d23347e068f93
estiens <eric@softwareforgood.com>
2018-06-21 07:16:17 -0500

    runaway memory

--- lib/new_scraper_clock.rb
+++ lib/new_scraper_clock.rb
@@ -10,7 +10,7 @@ module Clockwork
     waiting = rand(30)
     puts "waiting #{waiting}"
     sleep(waiting)
-    @runner ||= Scrapers::ScraperTasks.new
+    @runner = Scrapers::ScraperTasks.new
     @runner.check_for_live_game unless Match.in_progress.count.positive?
     @runner.scrape_for_events
     @runner.scrape_for_stats
