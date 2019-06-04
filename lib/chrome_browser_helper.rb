45c3c288e85881bf83c62ddf46e607d830b63abb
Eric Stiens <estiens@gmail.com>
2019-06-04 18:18:19 -0500

    and set chromedriver version explicitly on heroku?

--- lib/chrome_browser_helper.rb
+++ lib/chrome_browser_helper.rb
@@ -1,23 +1,22 @@
 module ChromeBrowserHelper
   def self.browser
-    if chrome_path = ENV['GOOGLE_CHROME_BIN']
-      Selenium::WebDriver::Chrome.path = chrome_path
-    end
     options = Selenium::WebDriver::Chrome::Options.new
-    driver = Selenium::WebDriver.for :chrome, options: options
     chrome_dir = Rails.root.join('tmp', 'chrome')
     FileUtils.mkdir_p(chrome_dir)
     user_data_dir = "--user-data-dir=#{chrome_dir}"
     options.add_argument user_data_dir
-    options.add_argument "window-size=800x600"
-    options.add_argument "headless"
-    options.add_argument "disable-gpu"
-    options.add_argument 'disable-setuid-sandbox'
-    options.add_argument 'disable-dev-shm-usage'
-    options.add_argument 'single-process'
+    options.add_argument '--window-size=800x600'
+    options.add_argument '--headless'
+    options.add_argument '--disable-gpu'
+    options.add_argument '--disable-setuid-sandbox'
+    options.add_argument '--disable-dev-shm-usage'
+    options.add_argument '--single-process'
+    options.add_argument '--remote-debugging-port=9222'
     if chrome_bin = ENV["GOOGLE_CHROME_SHIM"]
       options.add_argument "--no-sandbox"
       options.binary = chrome_bin
+      Webdrivers.logger.level = :DEBUG
+      Selenium::WebDriver::Chrome.path = ENV['GOOGLE_CHROME_BIN']
     end
     Watir::Browser.new :chrome, options: options
   end
