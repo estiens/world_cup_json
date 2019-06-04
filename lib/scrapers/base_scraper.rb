eab533bc75b01f2f6530ca4dd2b59e2f0caa9196
Eric Stiens <estiens@gmail.com>
2019-06-04 18:03:38 -0500

    ok, works locally, adding both paths to selenium on heroku

--- lib/scrapers/base_scraper.rb
+++ lib/scrapers/base_scraper.rb
@@ -5,31 +5,11 @@ module Scrapers
     attr_accessor :browser, :page
 
     def initialize
-      @browser = init_browser
+      @browser = ChromeBrowserHelper.browser
       @page = nil
     end
 
     private
-
-    def init_browser
-      options = Selenium::WebDriver::Chrome::Options.new
-      driver = Selenium::WebDriver.for :chrome, options: options
-      chrome_dir = Rails.root.join('tmp', 'chrome')
-      FileUtils.mkdir_p(chrome_dir)
-      user_data_dir = "--user-data-dir=#{chrome_dir}"
-      options.add_argument user_data_dir
-      options.add_argument "window-size=800x600"
-      options.add_argument "headless"
-      options.add_argument "disable-gpu"
-      options.add_argument 'disable-setuid-sandbox'
-      options.add_argument 'disable-dev-shm-usage'
-      options.add_argument 'single-process'
-      if chrome_bin = ENV["GOOGLE_CHROME_SHIM"]
-        options.add_argument "--no-sandbox"
-        options.binary = chrome_bin
-      end
-      Watir::Browser.new :chrome, options: options
-    end
 
     def scrape_page_from_url(before_events: false)
       raise 'Must set url' unless @url
