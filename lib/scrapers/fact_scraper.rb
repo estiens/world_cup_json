0aace7777703c22b03c5e5c433271cba1b487d0f
Eric Stiens <estiens@gmail.com>
2019-06-11 11:36:14 -0500

    fix stats url!

--- lib/scrapers/fact_scraper.rb
+++ lib/scrapers/fact_scraper.rb
@@ -32,7 +32,7 @@ module Scrapers
     private
 
     def scraper_url
-      base_url = 'https://www.fifa.com/worldcup/matches/match'
+      base_url = 'https://www.fifa.com/womensworldcup/matches/match'
       "#{base_url}/#{@match.fifa_id}/#match-statistics"
     end
 
